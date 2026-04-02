d446f78d1013d2090063f9f157ba563d97436521
epstory <epstory@thewildcats.net>
2026-04-02 12:02:58 -0400

    Added the entire evaluateExpression function, so now the calculator actually works!

--- src/CalculatorEngine.java
+++ src/CalculatorEngine.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.regex.Pattern;
 
 /**
@@ -58,14 +61,147 @@ public class CalculatorEngine {
         return "";
     }
 
+
+
+
+
+
+
     // Function to separate the expression by the operators
     public String[] evaluateExpression (String expression) {
+        String str = "expression";
+        String stringResult = "";
+        String delimiters = "[+/*-]";
+        String mult = "*";
+        String divi = "/";
+        String addi = "+";
+        String subt = "-";
+        int index = 0;
+        String[] separated = str.split("(?<=" + delimiters + ")|(?=" + delimiters + ")");
+        List<String> list = new ArrayList<>(Arrays.asList(separated));
+        System.out.println(list);
+        System.out.println("printing out integers into doubles...");
+        for (String x : list) {
+            index++;
+            if (!(x.equals(mult) | x.equals(divi) | x.equals(addi) | x.equals(subt))) {
+                double newX = Double.parseDouble(x);
+                System.out.println(newX);
+                list.set(index - 1, String.valueOf(newX));
+            }
+        }
 
-        // String to store the regular expression
-        //String regex = "*/+-";
-        // Splits the expression into an array of strings based on the operators in regex
-        String [] arrayOfNum = expression.split("[*/+-]");
+        index = 0;
+        while (list.size() > 1) {
+            System.out.println(list);
+            if (list.contains(mult)) {
+                System.out.println("found * in list");
+                index = 0;
+                for (String item : list) {
+                    System.out.println(item);
+                    if (!item.equals(mult)) {
+                        index++;
+                    } else {
+                        System.out.println("^ here it is");
+                        break;
+                    }
+                }
+                System.out.println("multiplying...");
+                int aPlace = index - 1;
+                int bPlace = index + 1;
+                String a = list.get(aPlace);
+                String b = list.get(bPlace);
+                double newA = Double.parseDouble(a);
+                double doubleA = newA;
+                double newB = Double.parseDouble(b);
+                double doubleB = newB;
+                double doubleResult = doubleA * doubleB;
+                stringResult = Double.toString(doubleResult);
+                list.set(aPlace, stringResult);
+                list.remove(bPlace);
+                list.remove(index);
+                System.out.println("the result is " + stringResult);
+                System.out.println(list);
+                continue;
 
-        return arrayOfNum;
-      }
+
+            } else if (list.contains(divi)) {
+                System.out.println("found / in list");
+                index = 0;
+                for (String item : list) {
+                    System.out.println(item);
+                    if (!item.equals(divi)) {
+                        index++;
+                    } else {
+                        System.out.println("^ here it is");
+                        break;
+                    }
+                }
+                System.out.println("dividing...");
+                int aPlace = index - 1;
+                int bPlace = index + 1;
+                String a = list.get(aPlace);
+                String b = list.get(bPlace);
+                double newA = Double.parseDouble(a);
+                double doubleA = newA;
+                double newB = Double.parseDouble(b);
+                double doubleB = newB;
+                double doubleResult = doubleA / doubleB;
+                stringResult = Double.toString(doubleResult);
+                list.set(aPlace, stringResult);
+                list.remove(bPlace);
+                list.remove(index);
+                System.out.println("the result is " + stringResult);
+                System.out.println(list);
+                continue;
+            }
+
+            index = 0;
+            for (int i = 0; i < list.size(); i++) {
+                String testItem = list.get(i);
+                index++;
+                switch (testItem) {
+                    case "+":
+                        System.out.println("found + in  list");
+                        System.out.println("adding...");
+                        int aPlace = index - 2;
+                        int bPlace = index + 0;
+                        String a = list.get(aPlace);
+                        String b = list.get(bPlace);
+                        double newA = Double.parseDouble(a);
+                        double doubleA = newA;
+                        double newB = Double.parseDouble(b);
+                        double doubleB = newB;
+                        double doubleResult = doubleA + doubleB;
+                        stringResult = Double.toString(doubleResult);
+                        list.set(aPlace, stringResult);
+                        list.remove(bPlace);
+                        list.remove(index - 1);
+                        System.out.println("the result is " + stringResult);
+                        System.out.println(list);
+                        continue;
+
+                    case "-":
+                        System.out.println("found - in list");
+                        System.out.println("subtracting...");
+                        aPlace = index - 2;
+                        bPlace = index + 0;
+                        a = list.get(aPlace);
+                        b = list.get(bPlace);
+                        newA = Double.parseDouble(a);
+                        doubleA = newA;
+                        newB = Double.parseDouble(b);
+                        doubleB = newB;
+                        doubleResult = doubleA - doubleB;
+                        stringResult = Double.toString(doubleResult);
+                        list.set(aPlace, stringResult);
+                        list.remove(bPlace);
+                        list.remove(index - 1);
+                        System.out.println("the result is " + stringResult);
+                        System.out.println(list);
+                        continue;
+                }
+            }
+        }
+    return new String[]{stringResult};
+    }
 }
